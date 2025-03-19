332a4c29520b8466d18d2a7949d9749406a4a9cc
edo <edouardlicn@gmail.com>
2025-03-19 12:19:10 +0000

    first data update

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A collection of wallpapers for Haiku
 
 These package provide wallpapers about nature.
 Include 4K and 1080p.
+
+Package distribute on CC-BY 4.0
---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Version
+VERSION="0.1-2"
+
+# Packname and Arch
+PACKAGE_BASE="haiku_wallpaper_nature"
+ARCH="any"
+
+# package name
+PACKAGE_NAME="${PACKAGE_BASE}-${VERSION}-${ARCH}.hpkg"
+
+# Build a package
+
+output=$(package create -b "$PACKAGE_NAME" 2>&1)
+exit_status=$?
+
+
+echo "About the package create command,system say:"
+echo
+echo "$output"
+
+
+if [ $exit_status -ne 0 ]; then
+    echo "package create failed,pls check again."
+    exit 1
+fi
+
+
+output=$(package add "$PACKAGE_NAME" data 2>&1)
+exit_status=$?
+
+
+echo "About the package add command,system say:"
+echo
+echo "$output"
+
+if [ $exit_status -ne 0 ]; then
+    echo "package add failed,pls check again."
+    exit 1
+fi
+    
+echo "All success,pls check your Haiku package."
